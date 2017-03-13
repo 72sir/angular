cedfe55732535b9b80edd75563c55345e84300e4
Alexandr Sirotkin <72gsir@gmail.com>
2017-03-14 00:35:33 +0300

    Create first projects

---
+++ app/app.js
@@ -0,0 +1,39 @@
+(function() {
+	'use strict';
+	
+	angular.module('app', []).controller('AppController', ['$scope', function($scope){
+		$scope.username = 'Leonard';
+		
+		$scope.userArray = [
+			['BB', 'King'],
+			['Ray', 'Charles'],
+			['Bobi', 'Bobility']
+		]
+		
+		$scope.userObject = [
+		{firstName: 'BB', lastName: 'King'},
+		{firstName: 'Json', lastName: 'Valin'},
+		{firstName: 'SIFI', lastName: 'LLLLLLL'},
+		]
+		
+		$scope.changeUsername = function(username) {
+			$scope.username = username;
+		}
+		
+		$scope.randomUserModel = 'random user';
+		
+		$scope.randomUserModelAlert = function() {
+			alert($scope.randomUserModel);
+		};
+		
+		$scope.textClass = '';
+		
+		$scope.changeTextClass = function(name) {
+			$scope.textClass = name;
+		}
+		
+		
+		
+		
+	}]);
+})();
